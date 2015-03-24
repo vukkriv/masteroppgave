72a6fa6421ad97d7df666a81bed58b20cd049188
jonhakr <jonhakr@stud.ntnu.no>
2015-03-24 17:38:34 +0100

    Maneuver/FormationCoordinator: Now listens for PlanControl requests for system to start/stop plan. If start, sends FormCoord Start if FormationControl was activated by plan. If stop, sends FormCoord Abort. Consumtion of FormCoord currently only sends PlanControl request for stop.
    config: Added Maneuver.FormationCoordinator to uav/Formation.ini

--- src/Maneuver/FormationCoordinator/Task.cpp
+++ src/Maneuver/FormationCoordinator/Task.cpp
@@ -22,7 +22,7 @@
 // language governing permissions and limitations at                        *
 // http://ec.europa.eu/idabc/eupl.html.                                     *
 //***************************************************************************
-// Author: Jon-Håkon Bøe Røli                                            *
+// Author: Jon-Håkon Bøe Røli                                               *
 //***************************************************************************
 
 // DUNE headers.
@@ -36,12 +36,23 @@ namespace Maneuver
 
     struct Task: public DUNE::Tasks::Task
     {
+      //! Formation coordination message
+      IMC::FormCoord m_form_coord;
+
+      IMC::PlanControl m_plan_ctrl;
+
       //! Constructor.
       //! @param[in] name task name.
       //! @param[in] ctx context.
       Task(const std::string& name, Tasks::Context& ctx):
         DUNE::Tasks::Task(name, ctx)
       {
+        //bind<IMC::PlanGeneration>(this);
+        bind<IMC::PlanControl>(this);
+        bind<IMC::PlanDB>(this);
+        //bind<IMC::PlanManeuver>(this);
+        //bind<IMC::PlanTransition>(this);
+        bind<IMC::FormCoord>(this);
       }
 
       //! Update internal state with new parameter values.
@@ -78,6 +89,184 @@ namespace Maneuver
       void
       onResourceRelease(void)
       {
+      }
+
+      void
+      consume(const IMC::PlanGeneration* msg)
+      {
+        trace("Got PlanGeneration \nfrom '%s' at '%s'",
+              resolveEntity(msg->getSourceEntity()).c_str(),
+              resolveSystemId(msg->getSource()));
+        msg->toText(std::cout);
+      }
+
+
+
+
+
+
+      void
+      consume(const IMC::PlanDB* msg)
+      {
+        trace("Got PlanDB \nfrom '%s' at '%s'",
+              resolveEntity(msg->getSourceEntity()).c_str(),
+              resolveSystemId(msg->getSource()));
+        //msg->toText(std::cout);
+
+        // Ignore if not reply for this entity
+        if (msg->getDestination() != getSystemId() || msg->getDestinationEntity() != getEntityId())
+        {
+          trace("Ignoring PlanDB with other destination");
+          return;
+        }
+
+        if (msg->type == IMC::PlanDB::DBT_SUCCESS && msg->op == IMC::PlanDB::DBOP_GET)
+        {
+          debug("Got plan from DB, checking if Formation Controller is activated...");
+          // Check if plan has activated Formation Controller
+          bool is_formation_control = false;
+          try
+            {
+              const IMC::SetEntityParameters* t_sep;
+              const IMC::PlanSpecification* planspec = static_cast<const IMC::PlanSpecification*>(msg->arg.get());
+              IMC::MessageList<IMC::PlanManeuver>::const_iterator it = planspec->maneuvers.begin();
+              for (; it != planspec->maneuvers.end(); it++ )
+              {
+                IMC::MessageList<IMC::Message>::const_iterator it_sa = (*it)->start_actions.begin();
+                for (; it_sa != (*it)->start_actions.end(); it_sa++ )
+                {
+                  t_sep = static_cast<const IMC::SetEntityParameters*>(*it_sa);
+                  if (t_sep->name.compare("Formation Controller") == 0)
+                  {
+                    IMC::MessageList<IMC::EntityParameter>::const_iterator it_ep = t_sep->params.begin();
+                    for (; it_ep != t_sep->params.end(); it_ep++ )
+                    {
+                      if ((*it_ep)->name.compare("Active") == 0 &&
+                          (*it_ep)->value.compare("true") == 0)
+                      {
+                        is_formation_control = true;
+                        break;
+                      }
+                    }
+                  }
+                  if (is_formation_control)
+                    break;
+                }
+                if (is_formation_control)
+                  break;
+              }
+            }
+            catch (std::exception& e)
+            {
+              err(DTR("Plan specification request failed with uncaught exception: %s"), e.what());
+            }
+
+            if (!is_formation_control)
+            {
+              debug("Plan did not activate Formation Controller");
+              return;
+            }
+
+            debug("Plan started with Formation Controller active!");
+            // Notify vehicles in formation
+            m_form_coord.flag = IMC::FormCoord::FC_START;
+            dispatch(m_form_coord);
+            debug("Sent Formation Start");
+        }
+
+      }
+
+      void
+      consume(const IMC::PlanControl* msg)
+      {
+        trace("Got PlanControl \nfrom '%s' at '%s'",
+              resolveEntity(msg->getSourceEntity()).c_str(),
+              resolveSystemId(msg->getSource()));
+        msg->toText(std::cout);
+
+        // Check if system was destination
+        if (msg->getDestination() != this->getSystemId())
+        {
+          trace("Ignored PlanControl with wrong destination '%s'",
+                resolveSystemId(msg->getDestination()));
+          return;
+        }
+
+        // Check if it is a request
+        if (msg->type != IMC::PlanControl::PC_REQUEST)
+        {
+          trace("Ignored PlanControl (not a request)");
+          return;
+        }
+
+        if (msg->op == IMC::PlanControl::PC_START)
+        {
+          debug("PlanControl START, requesting plan from DB...");
+          // Request plan details from Plan Database
+          IMC::PlanDB plan_db;
+          plan_db.type = IMC::PlanDB::DBT_REQUEST;
+          plan_db.op = IMC::PlanDB::DBOP_GET;
+          plan_db.plan_id = msg->plan_id;
+          dispatch(plan_db);
+        }
+        else if (msg->op == IMC::PlanControl::PC_STOP)
+        {
+          debug("Plan stopped");
+          // Notify vehicles in formation
+          m_form_coord.flag = IMC::FormCoord::FC_ABORT;
+          dispatch(m_form_coord);
+          debug("Sent Formation Abort");
+        }
+      }
+
+
+
+
+
+
+      void
+      consume(const IMC::PlanManeuver* msg)
+      {
+        trace("Got PlanManeuver \nfrom '%s' at '%s'",
+              resolveEntity(msg->getSourceEntity()).c_str(),
+              resolveSystemId(msg->getSource()));
+        msg->toText(std::cout);
+      }
+
+      void
+      consume(const IMC::PlanTransition* msg)
+      {
+        trace("Got PlanTransition \nfrom '%s' at '%s'",
+              resolveEntity(msg->getSourceEntity()).c_str(),
+              resolveSystemId(msg->getSource()));
+        msg->toText(std::cout);
+      }
+
+
+      void
+      consume(const IMC::FormCoord* msg)
+      {
+        // Ignored if sent by self
+        if (msg->getSource() == getSystemId())
+          return;
+
+        switch (msg->flag)
+        {
+          case IMC::FormCoord::FC_START:
+            debug("Received Formation Start");
+            //TODO: Load and execute plan where Formation Controller is activated
+            break;
+          case IMC::FormCoord::FC_ABORT:
+            war("Received Formation Abort");
+
+            m_plan_ctrl.type = IMC::PlanControl::PC_REQUEST;
+            m_plan_ctrl.op = IMC::PlanControl::PC_STOP;
+            dispatch(m_plan_ctrl);
+            break;
+          case IMC::FormCoord::FC_FINISHED:
+            debug("Received Formation Finished");
+            break;
+        }
       }
 
       //! Main loop.
