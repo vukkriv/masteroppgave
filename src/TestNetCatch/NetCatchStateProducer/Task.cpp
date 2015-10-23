2b360d9ef00d04d88424fd6e7532b570fd9f50e5
Jostein B. Moe <josteibm@stud.ntnu.no>
2015-10-23 15:41:35 +0200

    TestNetCatch/NetCatchStateProducer/: Test task to dispatch states

---
+++ src/TestNetCatch/NetCatchStateProducer/Task.cpp
@@ -0,0 +1,130 @@
+//***************************************************************************
+// Copyright 2007-2015 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
+//                                                                          *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// http://ec.europa.eu/idabc/eupl.html.                                     *
+//***************************************************************************
+// Author: Jostein B. Moe                                                   *
+//***************************************************************************
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+namespace TestNetCatch
+{
+  namespace NetCatchStateProducer
+  {
+    using DUNE_NAMESPACES;
+    //! %Task arguments.
+    struct Arguments
+    {
+      //! Frequency of output
+      double freq;
+      fp32_t north;
+      fp32_t east;
+      fp32_t down;
+    };
+
+    //! Last Estimated State received
+    IMC::EstimatedState m_estate;
+
+    struct Task: public DUNE::Tasks::Periodic
+    {
+      //! Task arguments.
+      Arguments m_args;
+            
+      //! Constructor.
+      //! @param[in] name task name.
+      //! @param[in] ctx context.
+      Task(const std::string& name, Tasks::Context& ctx):
+        DUNE::Tasks::Periodic(name, ctx)
+      {
+        param("Frequency", m_args.freq)
+        .description("Frequency of task")
+        .defaultValue("0.0");
+
+        param("North", m_args.north)
+        .units(Units::Meter)
+        .description("Default position in north");
+        
+        param("East", m_args.east)
+        .units(Units::Meter)
+        .description("Default position in east");
+
+        param("Down", m_args.down)
+        .units(Units::Meter)
+        .description("Default position in down");
+
+        this->setFrequency(m_args.freq);
+      }
+
+      //! Update internal state with new parameter values.
+      void
+      onUpdateParameters(void)
+      {
+      }
+
+      //! Reserve entity identifiers.
+      void
+      onEntityReservation(void)
+      {
+        inf("Starting: %s", resolveEntity(getEntityId()).c_str());
+      }
+
+      //! Resolve entity names.
+      void
+      onEntityResolution(void)
+      {
+      }
+
+      //! Acquire resources.
+      void
+      onResourceAcquisition(void)
+      {
+      }
+
+      //! Initialize resources.
+      void
+      onResourceInitialization(void)
+      {
+      }
+
+      //! Release resources.
+      void
+      onResourceRelease(void)
+      {
+      }
+
+      //! Main loop.
+      void
+      task(void)
+      {
+          m_estate.x = m_args.north;
+          m_estate.y = m_args.east;
+          m_estate.z = m_args.down;
+          m_estate.setSourceEntity(getEntityId());
+          dispatch(m_estate);
+      }
+    };
+  }
+}
+
+DUNE_TASK
