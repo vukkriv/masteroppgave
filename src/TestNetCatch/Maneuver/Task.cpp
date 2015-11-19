55db37357b3cd09ecc69c00fabfda0755983a743
Kristian Klausen <kristian.klausen@itk.ntnu.no>
2015-11-19 17:06:06 +0100

    TestNetCatch/Maneuver: Now dispatches a Plan with the netrecovery maneuver

--- src/TestNetCatch/Maneuver/Task.cpp
+++ src/TestNetCatch/Maneuver/Task.cpp
@@ -162,7 +162,64 @@ namespace TestNetCatch
     	  msg.max_acc	  = m_args.m_macc;
     	  msg.z_off		  = m_args.m_zoff;
 
-    	  dispatch(msg);
+    	  IMC::Goto goto_man;
+    	  goto_man.lat = msg.start_lat;
+    	  goto_man.lon = msg.start_lon;
+    	  goto_man.z   = msg.z;
+    	  goto_man.z_units = msg.z_units;
+    	  goto_man.speed = msg.speed;
+    	  goto_man.speed_units = IMC::SUNITS_METERS_PS;
+
+    	  IMC::PlanManeuver* pman1 = new IMC::PlanManeuver();
+    	  IMC::InlineMessage<IMC::Maneuver> pman1_inline;
+    	  pman1_inline.set(goto_man);
+    	  pman1->maneuver_id = "1";
+    	  pman1->data = pman1_inline;
+
+    	  IMC::PlanManeuver* pman2  = new IMC::PlanManeuver();
+    	  IMC::InlineMessage<IMC::Maneuver> pman2_inline;
+    	  pman2_inline.set(msg);
+    	  pman2->maneuver_id = "2";
+    	  pman2->data = pman2_inline;
+
+
+        IMC::PlanSpecification plan;
+
+        IMC::PlanTransition* transition = new IMC::PlanTransition;
+        transition->source_man = "1";
+        transition->dest_man   = "2";
+
+        plan.description = "A net recovery test plan";
+        plan.plan_id = "NetRecoveryTest";
+
+
+        IMC::MessageList<IMC::PlanTransition> translist;
+        translist.push_back(transition);
+
+        IMC::MessageList<IMC::PlanManeuver> manlist;
+        manlist.push_back(pman1);
+        manlist.push_back(pman2);
+
+        plan.start_man_id = "1";
+        plan.maneuvers = manlist;
+        plan.transitions = translist;
+
+        // Create plan
+        IMC::PlanControl planCtrl;
+        planCtrl.op = IMC::PlanControl::PC_START;
+        planCtrl.type = IMC::PlanControl::PC_REQUEST;
+        planCtrl.plan_id = "NetRecoveryTest";
+        planCtrl.request_id = 10;
+
+
+        // Apply custom maneuver to plan
+        IMC::InlineMessage<IMC::Message> arg;
+        arg.set(plan);
+        planCtrl.arg = arg;
+        debug("Custom plan activated");
+        dispatch(planCtrl);
+
+    	  //dispatch(msg);
     	  inf("NetRecovery maneuver message dispatched");
     	  maneuver_enabled = true;
       }
