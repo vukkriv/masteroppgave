ed00662215f8ec057e9e5d686009fed1450f4d60
Sigurd Olav Nevstad <sigurdon@stud.ntnu.no>
2016-03-04 10:04:48 +0100

    Control/Path/HeightGlideslope: Correct Indentation

--- src/Control/Path/HeightGlideslope/Task.cpp
+++ src/Control/Path/HeightGlideslope/Task.cpp
@@ -45,24 +45,24 @@ namespace Control
 
       struct Arguments
       {
-    	  bool use_controller; //Flag to enable controller
-          double k_vr;
-          double phi_h;
-          double k_ph_down;
-          double k_ih_down;
-          double k_r_down;
+        bool use_controller; //Flag to enable controller
+        double k_vr;
+        double phi_h;
+        double k_ph_down;
+        double k_ih_down;
+        double k_r_down;
 
-          double k_ph_up;
-          double k_ih_up;
-          double k_r_up;
+        double k_ph_up;
+        double k_ih_up;
+        double k_r_up;
 
-          double k_ph_line;
-          double k_ih_line;
-          double k_r_line;
+        double k_ph_line;
+        double k_ih_line;
+        double k_r_line;
 
-          double kp;
-          double h_dot_i;
-          double h_dot_p;
+        double kp;
+        double h_dot_i;
+        double h_dot_p;
 
       };
 
@@ -113,62 +113,62 @@ namespace Control
           m_shifting_waypoint(false)
 
         {
-        	param("Height bandwidth", m_args.phi_h)
-        			.units(Units::Meter)
-        			.defaultValue("20")
-        			.description("Limit distance above and bellow desired height from which maximum control is used");
+          param("Height bandwidth", m_args.phi_h)
+        			        .units(Units::Meter)
+        			        .defaultValue("20")
+        			        .description("Limit distance above and bellow desired height from which maximum control is used");
 
-            param("Vertical Rate maximum gain", m_args.k_vr)
-               .defaultValue("0.15")
-               .description("Vertical Rate maximum gain for control");
+          param("Vertical Rate maximum gain", m_args.k_vr)
+          .defaultValue("0.15")
+          .description("Vertical Rate maximum gain for control");
 
-            param("LOS Proportional gain up", m_args.k_ph_up)
-               .defaultValue("0.9")
-               .description("LOS Proportional gain for control");
+          param("LOS Proportional gain up", m_args.k_ph_up)
+          .defaultValue("0.9")
+          .description("LOS Proportional gain for control");
 
-            param("LOS Integral gain up", m_args.k_ih_up)
-               .defaultValue("0.1")
-               .description("LOS Integral gain for control");
+          param("LOS Integral gain up", m_args.k_ih_up)
+          .defaultValue("0.1")
+          .description("LOS Integral gain for control");
 
-            param("LOS Radius up", m_args.k_r_up)
-               .defaultValue("14.0")
-               .description("Approach distance gain up");
+          param("LOS Radius up", m_args.k_r_up)
+          .defaultValue("14.0")
+          .description("Approach distance gain up");
 
-            param("LOS Proportional gain down", m_args.k_ph_down)
-               .defaultValue("0.9")
-               .description("LOS Proportional gain for control");
+          param("LOS Proportional gain down", m_args.k_ph_down)
+          .defaultValue("0.9")
+          .description("LOS Proportional gain for control");
 
-            param("LOS Integral gain down", m_args.k_ih_down)
-               .defaultValue("0.1")
-               .description("LOS Integral gain for control");
+          param("LOS Integral gain down", m_args.k_ih_down)
+          .defaultValue("0.1")
+          .description("LOS Integral gain for control");
 
-            param("LOS Radius down", m_args.k_r_down)
-                .defaultValue("14.0")
-                .description("Approach distance gain up");
+          param("LOS Radius down", m_args.k_r_down)
+          .defaultValue("14.0")
+          .description("Approach distance gain up");
 
-            param("LOS Proportional gain line", m_args.k_ph_line)
-               .defaultValue("1.4")
-               .description("LOS Proportional gain for control");
+          param("LOS Proportional gain line", m_args.k_ph_line)
+          .defaultValue("1.4")
+          .description("LOS Proportional gain for control");
 
-            param("LOS Integral gain line", m_args.k_ih_line)
-               .defaultValue("0.02")
-               .description("LOS Integral gain for control");
+          param("LOS Integral gain line", m_args.k_ih_line)
+          .defaultValue("0.02")
+          .description("LOS Integral gain for control");
 
-            param("LOS Radius line", m_args.k_r_line)
-                .defaultValue("25.0")
-                .description("Approach distance gain up");
+          param("LOS Radius line", m_args.k_r_line)
+          .defaultValue("25.0")
+          .description("Approach distance gain up");
 
-            param("Use controller", m_args.use_controller)
-			  .visibility(Tasks::Parameter::VISIBILITY_USER)
-			  .scope(Tasks::Parameter::SCOPE_MANEUVER)
-			  .defaultValue("false")
-			  .description("Use this controller for maneuver");
+          param("Use controller", m_args.use_controller)
+          .visibility(Tasks::Parameter::VISIBILITY_USER)
+          .scope(Tasks::Parameter::SCOPE_MANEUVER)
+          .defaultValue("false")
+          .description("Use this controller for maneuver");
 
           bind<IMC::IndicatedSpeed>(this);
           //bind<IMC::DesiredPath>(this);
           //bind<IMC::DesiredZ>(this);
 
-       }
+        }
 
         void
         onPathActivation(void)
@@ -197,107 +197,107 @@ namespace Control
         void
         step(const IMC::EstimatedState& state, const TrackingState& ts)
         {
-            if (!m_args.use_controller)
-              return;
+          if (!m_args.use_controller)
+            return;
 
-            //Waypoint- handling
-            double start_z = ts.start.z;
-            double end_z = ts.end.z;
-
-
-            //Handle tracking to first waypoint: Need height offset in start.z for the first waypoint, W.I.P
-            if(start_z == last_end_z){
-            	first_waypoint = false;
-            }
-
-           if(first_waypoint){
-           	start_z = state.height - start_z;
-           }
-
-           double Vg = sqrt( (state.vx*state.vx) + (state.vy*state.vy) + (state.vz*state.vz) ); // Ground speed
+          //Waypoint- handling
+          double start_z = ts.start.z;
+          double end_z = ts.end.z;
 
 
-            // Calculate glide-slope angle
-            glideslope_angle = atan2((abs(end_z) -abs(start_z)),ts.track_length); //Negative for decent
-            double glideslope_angle_nofilter = glideslope_angle;
+          //Handle tracking to first waypoint: Need height offset in start.z for the first waypoint, W.I.P
+          if(start_z == last_end_z){
+            first_waypoint = false;
+          }
+
+          if(first_waypoint){
+            start_z = state.height - start_z;
+          }
+
+          double Vg = sqrt( (state.vx*state.vx) + (state.vy*state.vy) + (state.vz*state.vz) ); // Ground speed
 
 
-            //Calculate Z_ref based along-track along the glideslope. Endpoint is trimmed in order so Z_ref always is between the waypoints
-            if(abs(start_z) < abs(end_z)){//Glide-slope upwards
-            	zref.value = tan(glideslope_angle)*(ts.track_length - ts.range) + abs(start_z); //Current desired z
-            	zref.value = trimValue(zref.value,abs(start_z),tan(glideslope_angle)*(ts.track_length) + abs(start_z));
-            }
-            else{ //Glide-slope downwards
-            	zref.value = tan(glideslope_angle)*(ts.track_length - ts.range) + abs(start_z); //Current desired z
-            	zref.value = trimValue(zref.value,tan(glideslope_angle)*(ts.track_length)+ abs(start_z),abs(start_z));
-            }
-
-            if (m_first_run){
-            	desired_z_last = zref.value;
-                m_first_run = false;
-            }
-
-            //LP-Filter for desired z. Prevents jump in Z_ref in transition to tracking a new waypoint!
-            double lp_degree = 0.97;
-            desired_z_last = lp_degree*desired_z_last + (1-lp_degree)*zref.value;
-            last_glideslope_angle =0.99*last_glideslope_angle + (1-0.99)*glideslope_angle;
-            zref.value = desired_z_last;
-            glideslope_angle = last_glideslope_angle;
+          // Calculate glide-slope angle
+          glideslope_angle = atan2((abs(end_z) -abs(start_z)),ts.track_length); //Negative for decent
+          double glideslope_angle_nofilter = glideslope_angle;
 
 
-            double h_error = (zref.value - (state.height - state.z))*cos(glideslope_angle); // H_error w.r.t flight-path
+          //Calculate Z_ref based along-track along the glideslope. Endpoint is trimmed in order so Z_ref always is between the waypoints
+          if(abs(start_z) < abs(end_z)){//Glide-slope upwards
+            zref.value = tan(glideslope_angle)*(ts.track_length - ts.range) + abs(start_z); //Current desired z
+            zref.value = trimValue(zref.value,abs(start_z),tan(glideslope_angle)*(ts.track_length) + abs(start_z));
+          }
+          else{ //Glide-slope downwards
+            zref.value = tan(glideslope_angle)*(ts.track_length - ts.range) + abs(start_z); //Current desired z
+            zref.value = trimValue(zref.value,tan(glideslope_angle)*(ts.track_length)+ abs(start_z),abs(start_z));
+          }
 
-            inf("H_error: %f",h_error);
+          if (m_first_run){
+            desired_z_last = zref.value;
+            m_first_run = false;
+          }
 
-            //Integrator
-            double timestep = m_last_step.getDelta();
-            m_integrator = m_integrator + timestep*h_error;
-            m_integrator = trimValue(m_integrator,-2,2); //Anti wind-up at 2 degrees
+          //LP-Filter for desired z. Prevents jump in Z_ref in transition to tracking a new waypoint!
+          double lp_degree = 0.97;
+          desired_z_last = lp_degree*desired_z_last + (1-lp_degree)*zref.value;
+          last_glideslope_angle =0.99*last_glideslope_angle + (1-0.99)*glideslope_angle;
+          zref.value = desired_z_last;
+          glideslope_angle = last_glideslope_angle;
 
 
-            //double h_app = Vg*m_args.k_hv; //LOS Approach distance
+          double h_error = (zref.value - (state.height - state.z))*cos(glideslope_angle); // H_error w.r.t flight-path
+
+          inf("H_error: %f",h_error);
+
+          //Integrator
+          double timestep = m_last_step.getDelta();
+          m_integrator = m_integrator + timestep*h_error;
+          m_integrator = trimValue(m_integrator,-2,2); //Anti wind-up at 2 degrees
+
+
+          //double h_app = Vg*m_args.k_hv; //LOS Approach distance
           //  double h_error_trimmed = trimValue(abs(h_error),0,m_args.k_r); //Force the look-ahead distance to be within a circle with radius m_args.k_r
           //  double h_app = sqrt(m_args.k_r*m_args.k_r - h_error_trimmed*h_error_trimmed);
 
 
-            //Calculate look-ahead distance based on glide-slope up, down or straight line
-            if(glideslope_angle_nofilter > 0){ //Glideslope up
-            	double h_error_trimmed = trimValue(abs(h_error),0.0,m_args.k_r_up-0.5); //Force the look-ahead distance to be within a circle with radius m_args.k_r
-            	double h_app = sqrt(m_args.k_r_up*m_args.k_r_up - h_error_trimmed*h_error_trimmed);
-            	los_angle = atan2(m_args.k_ph_up*h_error + m_args.k_ih_up*m_integrator,h_app); //Calculate LOS-angle glideslope up
-            	inf("Glideslope UP! %f",glideslope_angle);
-            }
-            else if(glideslope_angle_nofilter < 0){ //Glideslope down
-            	double h_error_trimmed = trimValue(abs(h_error),0.0,m_args.k_r_down-0.5); //Force the look-ahead distance to be within a circle with radius m_args.k_r
-            	double h_app = sqrt(m_args.k_r_down*m_args.k_r_down - h_error_trimmed*h_error_trimmed);
-            	los_angle = atan2(m_args.k_ph_down*h_error + m_args.k_ih_down*m_integrator,h_app); //Calculate LOS-angle glideslope down
-            	inf("Glideslope DOWN! %f",glideslope_angle);
-            }
-            else{//Straight line
-            	double h_error_trimmed = trimValue(abs(h_error),0.0,m_args.k_r_line-0.5); //Force the look-ahead distance to be within a circle with radius m_args.k_r
-            	double h_app = sqrt(m_args.k_r_line*m_args.k_r_line - h_error_trimmed*h_error_trimmed);
-            	los_angle = atan2(m_args.k_ph_line*h_error + m_args.k_ih_line*m_integrator,h_app); //Calculate LOS-angle glideslope down
-            	inf("Glideslope LINE ! %f",glideslope_angle);
-            }
-            //Limit los_angle based on saturation limit for climb-rate.
-            los_angle = trimValue(los_angle,-0.15,0.15);
-            inf("Los_angle: %f",los_angle*(180/3.14159265));
+          //Calculate look-ahead distance based on glide-slope up, down or straight line
+          if(glideslope_angle_nofilter > 0){ //Glideslope up
+            double h_error_trimmed = trimValue(abs(h_error),0.0,m_args.k_r_up-0.5); //Force the look-ahead distance to be within a circle with radius m_args.k_r
+            double h_app = sqrt(m_args.k_r_up*m_args.k_r_up - h_error_trimmed*h_error_trimmed);
+            los_angle = atan2(m_args.k_ph_up*h_error + m_args.k_ih_up*m_integrator,h_app); //Calculate LOS-angle glideslope up
+            inf("Glideslope UP! %f",glideslope_angle);
+          }
+          else if(glideslope_angle_nofilter < 0){ //Glideslope down
+            double h_error_trimmed = trimValue(abs(h_error),0.0,m_args.k_r_down-0.5); //Force the look-ahead distance to be within a circle with radius m_args.k_r
+            double h_app = sqrt(m_args.k_r_down*m_args.k_r_down - h_error_trimmed*h_error_trimmed);
+            los_angle = atan2(m_args.k_ph_down*h_error + m_args.k_ih_down*m_integrator,h_app); //Calculate LOS-angle glideslope down
+            inf("Glideslope DOWN! %f",glideslope_angle);
+          }
+          else{//Straight line
+            double h_error_trimmed = trimValue(abs(h_error),0.0,m_args.k_r_line-0.5); //Force the look-ahead distance to be within a circle with radius m_args.k_r
+            double h_app = sqrt(m_args.k_r_line*m_args.k_r_line - h_error_trimmed*h_error_trimmed);
+            los_angle = atan2(m_args.k_ph_line*h_error + m_args.k_ih_line*m_integrator,h_app); //Calculate LOS-angle glideslope down
+            inf("Glideslope LINE ! %f",glideslope_angle);
+          }
+          //Limit los_angle based on saturation limit for climb-rate.
+          los_angle = trimValue(los_angle,-0.15,0.15);
+          inf("Los_angle: %f",los_angle*(180/3.14159265));
 
-            double gamma_cmd = glideslope_angle + los_angle; //Commanded flight path angle
-            double h_dot_desired = Vg*sin(gamma_cmd);		 //Convert commanded flight path angle to demanded vertical-rate.
+          double gamma_cmd = glideslope_angle + los_angle; //Commanded flight path angle
+          double h_dot_desired = Vg*sin(gamma_cmd);		 //Convert commanded flight path angle to demanded vertical-rate.
 
-            m_vrate.value = h_dot_desired;
+          m_vrate.value = h_dot_desired;
 
-            //Testing constant-rate
-            //h_dot_desired = Vg*sin(2*(M_PI/180));
-            //m_vrate.value= h_dot_desired;
+          //Testing constant-rate
+          //h_dot_desired = Vg*sin(2*(M_PI/180));
+          //m_vrate.value= h_dot_desired;
 
-        	dispatch(m_vrate);
-        	zref.z_units=Z_HEIGHT;
-        	dispatch(zref);
+          dispatch(m_vrate);
+          zref.z_units=Z_HEIGHT;
+          dispatch(zref);
 
-        	last_end_z = end_z;
-        	last_glideslope_angle = glideslope_angle;
+          last_end_z = end_z;
+          last_glideslope_angle = glideslope_angle;
 
         }
       };
