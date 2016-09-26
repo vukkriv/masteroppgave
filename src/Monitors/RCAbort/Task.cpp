cae672ea861f8c98073aaadc994f14a88ce00678
Kristian Klausen <kristian.klausen@itk.ntnu.no>
2016-09-26 15:17:42 +0200

    Monitors/RCAbort: New task that monitors a given channel for a pwm threshold, and sends an Abort message on trigger.

---
+++ src/Monitors/RCAbort/Task.cpp
@@ -0,0 +1,195 @@
+//***************************************************************************
+// Copyright 2007-2016 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
+//                                                                          *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// http://ec.europa.eu/idabc/eupl.html.                                     *
+//***************************************************************************
+// Author: Kristian Klausen                                                 *
+//***************************************************************************
+// This task monitors a certain PWM value on a certain channel, and issues abort when
+// the value reaches a certain threshold
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+namespace Monitors
+{
+  namespace RCAbort
+  {
+    using DUNE_NAMESPACES;
+
+    struct Arguments
+    {
+      int channel;
+      bool trigger_on_high;
+      double threshold;
+      double time_between_aborts;
+
+    };
+
+    struct Task: public DUNE::Tasks::Task
+    {
+      //! Task arguments
+      Arguments m_args;
+
+      //! Time last abort was sent.
+      double m_time_prev_abort;
+
+      //! Previous pwm value on channel
+      int m_prev_duty_cycle;
+
+      //! Constructor.
+      //! @param[in] name task name.
+      //! @param[in] ctx context.
+      Task(const std::string& name, Tasks::Context& ctx):
+        DUNE::Tasks::Task(name, ctx),
+        m_prev_duty_cycle(1500)
+      {
+        param("Channel", m_args.channel)
+        .minimumValue("1")
+        .defaultValue("7")
+        .maximumValue("12")
+        .description("Channel [1-12] to trigger on. ");
+
+        param("Trigger On High", m_args.trigger_on_high)
+        .defaultValue("true")
+        .description("Set to true to trigger on higher than treshold. Set to false to trigger on lower than threshold.");
+
+        param("Threshold", m_args.threshold)
+        .minimumValue("900")
+        .defaultValue("1600")
+        .maximumValue("2100")
+        .description("PWM Value [900-2100] to trigger on. ");
+
+        param("Time Between Aborts", m_args.time_between_aborts)
+        .defaultValue("3")
+        .units(Units::Second)
+        .description("Time between consequtive aborts being sent .");
+
+        // Initiate previous abort time;
+        m_time_prev_abort = Clock::get();
+
+
+
+        bind<IMC::PWM>(this);
+      }
+
+      //! Update internal state with new parameter values.
+      void
+      onUpdateParameters(void)
+      {
+      }
+
+      //! Reserve entity identifiers.
+      void
+      onEntityReservation(void)
+      {
+      }
+
+      //! Resolve entity names.
+      void
+      onEntityResolution(void)
+      {
+      }
+
+      //! Acquire resources.
+      void
+      onResourceAcquisition(void)
+      {
+      }
+
+      //! Initialize resources.
+      void
+      onResourceInitialization(void)
+      {
+      }
+
+      //! Release resources.
+      void
+      onResourceRelease(void)
+      {
+      }
+
+      //! Consume PWM Values
+      void
+      consume(const IMC::PWM* pwm)
+      {
+        //! Check for channel
+        // ID is 1-indexed for both argument and message.
+        if( (pwm->id ) == m_args.channel)
+        {
+          spew("Got PWM packet on correct channel. ");
+          bool send_abort = false;
+
+          spew("PWM VALUE: %d, Threshold: %f", pwm->duty_cycle, m_args.threshold);
+
+          bool triggered = isTriggered(pwm->duty_cycle);
+          bool wasTriggered = isTriggered(m_prev_duty_cycle);
+
+          // Check for new trigger
+          if (triggered && !wasTriggered)
+            send_abort = true;
+
+          // Check for old trigger, and time.
+          if (triggered && Clock::get() > m_time_prev_abort + m_args.time_between_aborts)
+            send_abort = true;
+
+          if (send_abort)
+          {
+            IMC::Abort abort;
+            dispatch(abort);
+
+            m_time_prev_abort = Clock::get();
+
+            err("RC Monitor dispatched Abort");
+          }
+
+          // Update prev duty cycle
+          m_prev_duty_cycle = pwm->duty_cycle;
+        }
+      }
+
+      bool
+      isTriggered(int pwmValue)
+      {
+        if ( m_args.trigger_on_high && pwmValue > m_args.threshold)
+          return true;
+        if (!m_args.trigger_on_high && pwmValue < m_args.threshold)
+          return true;
+
+        return false;
+
+      }
+
+      //! Main loop.
+      void
+      onMain(void)
+      {
+        while (!stopping())
+        {
+          waitForMessages(1.0);
+        }
+      }
+    };
+  }
+}
+
+DUNE_TASK
