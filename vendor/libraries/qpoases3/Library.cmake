afd7e7e8263655aa8867d6922a81bde864511992
Kristian Klausen <kristian.klausen@itk.ntnu.no>
2017-08-29 15:20:12 +0200

    vendor: libraries/qpoases3: Added external library qpoases3.

---
+++ vendor/libraries/qpoases3/Library.cmake
@@ -0,0 +1,9 @@
+file(GLOB DUNE_QPOASES3_FILES user/vendor/libraries/qpoases3/*.c)
+
+set_source_files_properties(${DUNE_QPOASES3_FILES}
+    PROPERTIES COMPILE_FLAGS "${DUNE_C_FLAGS} ${DUNE_C_FLAGS_STRICT}")
+
+list(APPEND DUNE_VENDOR_FILES ${DUNE_QPOASES3_FILES})
+
+set(DUNE_VENDOR_INCS_DIR ${DUNE_VENDOR_INCS_DIR}
+  ${PROJECT_SOURCE_DIR}/user/vendor/libraries)
